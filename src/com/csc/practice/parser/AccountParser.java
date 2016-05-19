92639121af627db561f8f3aa5513dfd91928f70c
unknown <189993@tw.csc.com>
2016-05-19 13:47:03 +0800

    modify account regx

--- src/com/csc/practice/parser/AccountParser.java
+++ src/com/csc/practice/parser/AccountParser.java
@@ -30,7 +30,7 @@ public class AccountParser extends AbstractParser {
 		Map<String, Account> accountList = new HashMap<String, Account>();
 
 		try {
-			String pattern = "^(S+)\\,(\\S+)\\,(\\d+)\\,(S+)$";
+			String pattern = "^(\\S+)\\,(\\S+)\\,(\\d+)\\,(\\S+)$";
 			Pattern r = Pattern.compile(pattern);
 			List<String> accountStringList = readInput(filePath);
 			for (String line : accountStringList) {
